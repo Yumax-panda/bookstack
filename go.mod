58603468ead74dab1f4602e270ec37c069108c64
Yumax-panda <yuma0wall@gmail.com>
2024-03-10 02:54:57 +0900

    修正

--- go.mod
+++ go.mod
@@ -2,9 +2,17 @@ module bookstack
 
 go 1.22.1
 
-require github.com/labstack/echo v3.3.10+incompatible
+require (
+	github.com/google/uuid v1.6.0
+	github.com/labstack/echo v3.3.10+incompatible
+	gorm.io/driver/mysql v1.5.4
+	gorm.io/gorm v1.25.7
+)
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,45 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"bookstack/migration"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/labstack/echo"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func connectDB() *sql.DB {
+	dsn := getDBConfig()
+	var err error
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(fmt.Errorf("DB Error: %w", err))
+	}
+	migration.Migrate(db)
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(fmt.Errorf("DB Error: %w", err))
+	}
+	return sqlDB
+}
+
+func getDBConfig() string {
+	user := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	hostname := os.Getenv("DB_HOSTNAME")
+	port := os.Getenv("DB_PORT")
+	db_database := os.Getenv("DB_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, hostname, port, db_database) + "?charset=utf8mb4&parseTime=True&loc=Local"
+	return dsn
+}
 
 func main() {
+	sqlDB := connectDB()
+	defer sqlDB.Close()
 	e := echo.New()
 	e.Start("8000")
 }
