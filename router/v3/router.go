b5e2904fe56c01b4e68c546cc5514fd120611ff2
Yumax-panda <yuma0wall@gmail.com>
2024-03-17 23:10:44 +0900

    v3ルートを実装

--- router/v3/router.go
+++ router/v3/router.go
@@ -10,7 +10,7 @@ type Handlers struct {
 	Repo repository.Repository
 }
 
-func (h *Handlers) Setup(e echo.Group) {
+func (h *Handlers) Setup(e *echo.Group) {
 	api := e.Group("/v3")
 	{
 		apiUsers := api.Group("/users")
