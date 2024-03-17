b099a3f18c799490c1fd4f3a19d384f2f82f2468
Yumax-panda <yuma0wall@gmail.com>
2024-03-18 02:44:52 +0900

    サーバーが動くようにできた

--- cmd/config.go
+++ cmd/config.go
@@ -21,20 +21,20 @@ type Config struct {
 	} `mapstructure:"db" yaml:"db"`
 }
 
-func setDefaults() {
-	viper.SetDefault("db.host", "http://localhost:8000")
+func init() {
+	viper.SetDefault("origin", "http://localhost:3000")
 	viper.SetDefault("port", 8000)
+	viper.SetDefault("db.host", "mysql")
 	viper.SetDefault("db.port", 3306)
 	viper.SetDefault("db.username", "root")
 	viper.SetDefault("db.password", "password")
 	viper.SetDefault("db.database", "bookstack")
-	viper.SetDefault("origin", "http://localhost:3000")
 }
 
 func (c Config) getDatabase() (*gorm.DB, error) {
 	engine, err := gorm.Open(mysql.New(mysql.Config{
 		DSN: fmt.Sprintf(
-			"%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&collation=utf8mb4_general_ci&parseTime=true",
+			"%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 			c.DB.Username,
 			c.DB.Password,
 			c.DB.Host,
