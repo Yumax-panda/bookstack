6e5e92aac2e856c7d60bbeb5b10461386c8d1c1b
Yumax-panda <yuma0wall@gmail.com>
2024-03-19 00:29:51 +0900

    ユーザー作成のロジックを追加

--- repository/gorm/user.go
+++ repository/gorm/user.go
@@ -2,7 +2,13 @@ package gorm
 
 import (
 	"bookstack/model"
+	"bookstack/repository"
+	"bookstack/utils"
+	"bookstack/utils/gormutil"
+	"bookstack/utils/random"
+	"encoding/hex"
 
+	"github.com/gofrs/uuid"
 	"gorm.io/gorm"
 )
 
@@ -21,4 +27,44 @@ func (r *userRepository) FindAll() ([]model.User, error) {
 		return nil, err
 	}
 	return users, nil
+}
+
+// CreateUser implements UserRepository interface
+func (r *userRepository) CreateUser(args repository.CreateUserArgs) (*model.User, error) {
+	uid := uuid.Must(uuid.NewV4())
+	user := &model.User{
+		ID:          uid,
+		Name:        args.Name,
+		DisplayName: args.DisplayName,
+		Icon:        args.Icon,
+		Profile: &model.UserProfile{
+			UserID: uid,
+		},
+	}
+
+	if len(args.Password) > 0 {
+		salt := random.Salt()
+		user.Password = hex.EncodeToString(utils.HashPassword(args.Password, salt))
+		user.Salt = hex.EncodeToString(salt)
+	}
+
+	err := r.db.Transaction(func(tx *gorm.DB) error {
+		if exists, err := gormutil.RecordExists(tx, &model.User{Name: user.Name}); err != nil {
+			return err
+		} else if exists {
+			return repository.ErrAlreadyExists
+		}
+
+		// userとuser_profileを作成
+		if err := tx.Create(user).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
--- repository/user.go
+++ repository/user.go
@@ -2,6 +2,18 @@ package repository
 
 import "bookstack/model"
 
+type CreateUserArgs struct {
+	Name        string
+	DisplayName string
+	Icon        string
+	Password    string
+}
+
 type UserRepository interface {
 	FindAll() ([]model.User, error)
+	// CreateUser 新規ユーザーを作成
+	//
+	// 成功した場合、ユーザーとnilを返す。
+	// Nameが重複している場合、ErrAlreadyExistsを返す。
+	CreateUser(args CreateUserArgs) (*model.User, error)
 }
