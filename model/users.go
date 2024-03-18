deb840a10e52cf17ae3ec7eb83141416c491a84d
Yumax-panda <yuma0wall@gmail.com>
2024-03-18 23:31:25 +0900

    修正

--- model/users.go
+++ model/users.go
@@ -1,10 +1,26 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
 
 type User struct {
-	ID       uuid.UUID `json:"id"`
-	Name     string    `json:"name"`
-	Email    string    `json:"email"`
-	Password string    `json:"password"`
+	ID          uuid.UUID `gorm:"type:char(36);not null;primaryKey"`
+	Name        string    `gorm:"type:varchar(32);not null;unique"`
+	DisplayName string    `gorm:"type:varchar(32);not null;default:''"`
+	Password    string    `gorm:"type:char(128);not null;default:''"`
+	Salt        string    `gorm:"type:char(128);not null;default:''"`
+	Icon        uuid.UUID `gorm:"type:char(36);not null"`
+	CreatedAt   time.Time `gorm:"precision:6"`
+	UpdatedAt   time.Time `gorm:"precision:6"`
+
+	Profile *UserProfile `gorm:"constraint:user_profiles_user_id_users_id_foreign,OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+type UserProfile struct {
+	UserID    uuid.UUID `gorm:"type:char(36);not null;primaryKey"`
+	Bio       string    `gorm:"type:TEXT COLLATE utf8mb4_bin NOT NULL"`
+	UpdatedAt time.Time `gorm:"precision:6"`
 }
