76d4702bdd52aca4dd796c6d6a43f0d9412c0b56
Yumax-panda <yuma0wall@gmail.com>
2024-03-19 22:43:15 +0900

    権限を実装したい

---
+++ service/rbac/permission/permission.go
@@ -0,0 +1,47 @@
+package permission
+
+// Permission パーミッション
+type Permission string
+
+// Name パーミッション名
+func (p Permission) Name() string {
+	return string(p)
+}
+
+// Permissions パーミッションセット
+type Permissions map[Permission]struct{}
+
+// Add セットに権限を追加します
+func (set Permissions) Add(p Permission) {
+	set[p] = struct{}{}
+}
+
+// Remove セットから権限を削除します
+func (set Permissions) Remove(p Permission) {
+	delete(set, p)
+}
+
+// Contains セットに指定した権限が含まれているかどうか
+func (set Permissions) Contains(p Permission) bool {
+	_, ok := set[p]
+	return ok
+}
+
+// Array セットの権限の配列を返します
+func (set Permissions) Array() []Permission {
+	result := make([]Permission, 0, len(set))
+	for k := range set {
+		result = append(result, k)
+	}
+	return result
+}
+
+func PermissionsFromArray(perms []Permission) Permissions {
+	res := Permissions{}
+	for _, perm := range perms {
+		res.Add(perm)
+	}
+	return res
+}
+
+var List = []Permission{}
