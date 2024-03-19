b89ab198d9d5e26c0014c2a294a356be372b2575
Yumax-panda <yuma0wall@gmail.com>
2024-03-20 01:31:14 +0900

    UserInfoインターフェースを実装

--- model/users.go
+++ model/users.go
@@ -1,10 +1,31 @@
 package model
 
 import (
+	"bookstack/utils"
+	"crypto/subtle"
+	"encoding/hex"
+	"errors"
 	"time"
 
 	"github.com/gofrs/uuid"
 )
+
+var (
+	// ErrUserWrongIDOrPassword : ユーザーエラー IDかパスワードが間違っています。
+	ErrUserWrongIDOrPassword = errors.New("password or id is wrong")
+)
+
+type UserInfo interface {
+	GetID() uuid.UUID
+	GetName() string
+	GetDisplayName() string
+	GetIcon() string
+	GetCreatedAt() time.Time
+	GetUpdatedAt() time.Time
+
+	GetBio() string
+	Authenticate(password string) error
+}
 
 type User struct {
 	ID          uuid.UUID `gorm:"type:char(36);not null;primaryKey"`
@@ -19,8 +40,65 @@ type User struct {
 	Profile *UserProfile `gorm:"constraint:user_profiles_user_id_users_id_foreign,OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
+func (User) TableName() string {
+	return "users"
+}
+
 type UserProfile struct {
 	UserID    uuid.UUID `gorm:"type:char(36);not null;primaryKey"`
 	Bio       string    `gorm:"type:TEXT COLLATE utf8mb4_bin NOT NULL"`
 	UpdatedAt time.Time `gorm:"precision:6"`
+}
+
+func (UserProfile) TableName() string {
+	return "user_profiles"
+}
+
+func (u *User) GetID() uuid.UUID {
+	return u.ID
+}
+
+func (u *User) GetName() string {
+	return u.Name
+}
+
+func (u *User) GetDisplayName() string {
+	return u.DisplayName
+}
+
+func (u *User) GetIcon() string {
+	return u.Icon
+}
+
+func (u *User) GetCreatedAt() time.Time {
+	return u.CreatedAt
+}
+
+func (u *User) GetUpdatedAt() time.Time {
+	return u.UpdatedAt
+}
+
+func (u *User) GetBio() string {
+	if u.Profile == nil {
+		panic("unexpected control flow: user profile is not loaded")
+	}
+	return u.Profile.Bio
+}
+
+func (u *User) Authenticate(password string) error {
+	if len(u.Password) == 0 || len(u.Salt) == 0 {
+		return ErrUserWrongIDOrPassword
+	}
+
+	storedPassword, err := hex.DecodeString(u.Password)
+
+	if err != nil {
+		return ErrUserWrongIDOrPassword
+	}
+
+	if subtle.ConstantTimeCompare(storedPassword, utils.HashPassword(password, []byte(u.Salt))) != 1 {
+		return ErrUserWrongIDOrPassword
+	}
+
+	return nil
 }
