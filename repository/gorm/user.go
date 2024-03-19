ceea3f72e164040dbce8e55c69f5bd2454c4c78c
Yumax-panda <yuma0wall@gmail.com>
2024-03-20 01:31:33 +0900

    cacheを使う

--- repository/gorm/user.go
+++ repository/gorm/user.go
@@ -6,31 +6,52 @@ import (
 	"bookstack/utils"
 	"bookstack/utils/gormutil"
 	"bookstack/utils/random"
+	"context"
 	"encoding/hex"
+	"time"
 
 	"github.com/gofrs/uuid"
+	"github.com/motoki317/sc"
 	"gorm.io/gorm"
 )
 
+type getUserArg struct {
+	id          uuid.UUID
+	withProfile bool
+}
+
+var _ repository.UserRepository = (*userRepository)(nil)
+
 type userRepository struct {
-	db *gorm.DB
+	db    *gorm.DB
+	users *sc.Cache[getUserArg, model.UserInfo]
 }
 
 func makeUserRepository(db *gorm.DB) *userRepository {
-	return &userRepository{db}
+	r := &userRepository{db: db}
+	r.users = sc.NewMust(r.getUser, 1*time.Hour, 1*time.Hour)
+	return r
 }
 
-// FindAll implements UserRepository interface
-func (r *userRepository) FindAll() ([]model.User, error) {
-	var users []model.User
-	if err := r.db.Find(&users).Error; err != nil {
-		return nil, err
+func (r *userRepository) getUser(_ context.Context, arg getUserArg) (model.UserInfo, error) {
+	tx := r.db
+	if arg.withProfile {
+		tx = tx.Preload("Profile")
 	}
-	return users, nil
+	var user model.User
+	if err := tx.First(&user, &model.User{ID: arg.id}).Error; err != nil {
+		return nil, convertError(err)
+	}
+	return &user, nil
+}
+
+func (r *userRepository) forgetCache(id uuid.UUID) {
+	r.users.Forget(getUserArg{id: id, withProfile: false})
+	r.users.Forget(getUserArg{id: id, withProfile: true})
 }
 
 // CreateUser implements UserRepository interface
-func (r *userRepository) CreateUser(args repository.CreateUserArgs) (*model.User, error) {
+func (r *userRepository) CreateUser(args repository.CreateUserArgs) (model.UserInfo, error) {
 	uid := uuid.Must(uuid.NewV4())
 	user := &model.User{
 		ID:          uid,
@@ -67,4 +88,11 @@ func (r *userRepository) CreateUser(args repository.CreateUserArgs) (*model.User
 	}
 
 	return user, nil
+}
+
+func (r *userRepository) GetUser(id uuid.UUID, withProfile bool) (model.UserInfo, error) {
+	if id == uuid.Nil {
+		return nil, repository.ErrNotFound
+	}
+	return r.users.Get(context.Background(), getUserArg{id, withProfile})
 }
