49c6074c0e0b2c1aabfba0b87e9d68199fb0a4b6
Yumax-panda <yuma0wall@gmail.com>
2024-03-20 22:15:42 +0900

    タグ名を追加

---
+++ router/consts/headers.go
@@ -0,0 +1,12 @@
+package consts
+
+const (
+	HeaderCacheControl      = "Cache-Control"
+	HeaderETag              = "ETag"
+	HeaderIfMatch           = "If-Match"
+	HeaderIfNoneMatch       = "If-None-Match"
+	HeaderIfModifiedSince   = "If-Modified-Since"
+	HeaderIfUnmodifiedSince = "If-Unmodified-Since"
+	HeaderMore              = "X-BOOKSTACK-More"
+	HeaderVersion           = "X-BOOKSTACK-VERSION"
+)
